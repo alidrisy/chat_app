b4306e8c3ca6230df830344b0040d95de3774b3b
Abdulrahman Alidrisy <a.alidrisy6@gmail.com>
2024-08-12 09:37:40 +0300

    create messages controller

--- backend/src/controllers/messages.controller.ts
+++ backend/src/controllers/messages.controller.ts
@@ -1,14 +1,51 @@
 import { Request, Response } from 'express';
+import prisma from '../db/prisma.js';
 
-export const getMessages = (req: Request, res: Response) => {
+export const sendMassege = async (req: Request, res: Response) => {
     try {
-        res.send('Messages route');
+        const { message } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user.id;
 
+        // Check if we have a conversation
+        let conversation = await prisma.conversation.findFirst({
+            where: {
+                participantsIds: {
+                    hasEvery: [senderId, receiverId],
+                },
+            },
+        });
+
+        if (!conversation) {
+            conversation = await prisma.conversation.create({
+                data: {
+                    participantsIds:{ 
+                        set: [senderId, receiverId],
+                    }
+                },
+            });
+        }
+        const newMessage = await prisma.message.create({
+            data: {
+                senderId,
+                conversationId: conversation.id,
+                body: message,
+            },
+        });
+
+        if (newMessage) {
+            conversation = await prisma.conversation.update({
+                where: { id: conversation.id },
+                data: {
+                    messages: {
+                        connect: { id: newMessage.id },
+                    },
+                },
+            });
+        }
+        res.status(201).json(newMessage);
     } catch (error: any) {
-        res.status(500).json({ message: error.message });
+        console.error(error);
+        res.status(500).json({ error: "Internal server erorr" });
     }
-};
-
-export const getConversations = (req: Request, res: Response) => {
-    res.send('Conversation route');
 };
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import protectRoute from '../middleware/protectRoute.js';
+import { sendMassege } from '../controllers/messages.controller.js';
 
 const router = express.Router();
 
-router.get('/conversations', (_req, res) => {
-    res.send('Conversation route');
-});
+router.post('/send/:id', protectRoute, sendMassege);
 
 router.get('/messages', (_req, res) => {
     res.send('Message route');
